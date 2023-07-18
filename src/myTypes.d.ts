ceee80c00a9c2f9c7d0592c5ef90b09a5d82311c
Diarmuid O Connor <doconnor@wit.ie>
2023-07-18 18:30:59 +0100

    Added Buddy types

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,24 +1,42 @@
-
 export interface Friend {
-    name: string;
-    phone: string;
-    dob? : Date;
-    age: number;
-    interests? : string[]
+  name: string;
+  phone: string;
+  dob?: Date;
+  age: number;
+  interests?: string[];
 }
 
-
 export interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-      email: string;
-      extension: number;
-      slack? : string
-    } 
-  }
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number;
+    slack?: string;
+  };
+}
 
-  export interface EmailContact {
-    name: string;
-    email: string
-  }
+export interface EmailContact {
+  name: string;
+  email: string;
+}
+
+export type Buddy = Friend | ColleagueV2;
+export type Department = "Engineering" | "Accounts" | "HR";
+export type Administrator = Buddy | string | undefined
+
+export type BuddyList = {
+  name: string;
+  administrator: Administrator;
+  members: Buddy[];
+};
+
+export type ColleagueV2 =  {
+  name: string;
+  department: Department;
+  contact: {
+    email: string;
+    extension: number;
+    slack?: string;
+  };
+}
